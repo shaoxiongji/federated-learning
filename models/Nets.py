c0eb66e643e5eebb3476c6892cf78c660a5bcd6e
delacruz878 <147753237+delacruz878@users.noreply.github.com>
2023-12-06 20:58:45 -0600

    Update Nets.py
    Added CNN model used for ASL & IMU datasets

--- models/Nets.py
+++ models/Nets.py
@@ -42,6 +42,45 @@ class CNNMnist(nn.Module):
         x = self.fc2(x)
         return x
 
+class CNNASL(nn.Module):
+    def __init__(self, args):
+        super(CNNASL, self).__init__()
+        self.conv1 = nn.Conv2d(args.num_channels, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(74420, 24)
+        #self.fc2 = nn.Linear(10000, 24)
+        #self.fc3 = nn.Linear(5000, 24)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, x.shape[1]*x.shape[2]*x.shape[3])
+        x = F.relu(self.fc1(x))
+        #x = F.dropout(x, training=self.training)
+        #x = self.fc2(x)
+        #x = F.dropout(x, training=self.training)
+        #x= self.fc3(x)
+        return x
+
+class CNNimu(nn.Module):
+    def __init__(self, args):
+        super(CNNimu, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=3)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=3)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 3)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, x.shape[1]*x.shape[2]*x.shape[3])
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return x
+
 
 class CNNCifar(nn.Module):
     def __init__(self, args):
